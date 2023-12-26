5b8cb9422a49136cff4034b3977d6b842e96ab7d
Khan Mubashshir Azeem <khanmubashshir08@gmail.com>
2023-12-26 10:43:00 +0530

    Signup error solved

--- app/src/main/java/com/example/foodie/Activity/SignUpActivity.java
+++ app/src/main/java/com/example/foodie/Activity/SignUpActivity.java
@@ -22,7 +22,7 @@ public class SignUpActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivitySignUpBinding.inflate(getLayoutInflater());
-        setContentView(R.layout.activity_sign_up);
+        setContentView(binding.getRoot());
 
         setVariable();
     }
@@ -31,36 +31,34 @@ public class SignUpActivity extends BaseActivity {
         binding.signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                try {
-                    String email = binding.emailSign.getText().toString();
-                    String password = binding.passwordSign.getText().toString();
 
-                    if (password.length() < 6) {
-                        Toast.makeText(SignUpActivity.this, "Your password must be 6 characters", Toast.LENGTH_SHORT).show();
-                        return;
-                    }
+                String email = binding.emailSign.getText().toString().trim();
+                String password = binding.passwordSign.getText().toString().trim();
 
-                    auth.createUserWithEmailAndPassword(email, password)
-                            .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if (task.isSuccessful()) {
-                                        Log.i(TAG, "onComplete: User creation successful");
-                                        Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
-                                        startActivity(intent);
-                                        finish();
-                                    } else {
-                                        Log.e(TAG, "onComplete: User creation failed", task.getException());
-                                        Toast.makeText(SignUpActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-                            });
-                } catch (Exception e) {
-                    Log.e(TAG, "Exception in onClick", e);
-                    // Handle the exception or log it as needed
-                    Toast.makeText(SignUpActivity.this, "An unexpected error occurred", Toast.LENGTH_SHORT).show();
+                if (password.length() < 6) {
+                    Toast.makeText(SignUpActivity.this, "Your password must be 6 characters", Toast.LENGTH_SHORT).show();
+                    return;
                 }
+
+                auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Log.i(TAG, "onComplete: User creation successful");
+                                    Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    Log.e(TAG, "onComplete: User creation failed", task.getException());
+                                    Toast.makeText(SignUpActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
             }
         });
     }
+
+
 }
